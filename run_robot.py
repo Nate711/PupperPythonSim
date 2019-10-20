935b2132f29f4b90b080545958024364f8f9079d
Sohit Gatiganti <sohitg@stanford.edu>
2019-10-20 23:35:28 +0100

    added joystick control

--- run_robot.py
+++ run_robot.py
@@ -10,6 +10,7 @@ from src.HardwareInterface import (
 from src.PupperConfig import MovementReference, GaitParams, StanceParams, SwingParams
 import time
 import numpy as np
+import UDPComms
 
 
 def main():
@@ -21,7 +22,7 @@ def main():
 
     controller = Controller()
     controller.movement_reference = MovementReference()
-    controller.movement_reference.v_xy_ref = np.array([0.0, 0.15])
+    controller.movement_reference.v_xy_ref = np.array([0.0, 0.0])
     controller.movement_reference.wz_ref = 0
     controller.swing_params = SwingParams()
     controller.swing_params.z_clearance = 0.02
@@ -32,6 +33,7 @@ def main():
 
     initialize_pwm(pi_board, pwm_params)
 
+    values = UDPComms.Subscriber(8830)
     last_loop = time.time()
     now = last_loop
     start = time.time()
@@ -39,7 +41,9 @@ def main():
         last_loop = time.time()
         step_controller(controller)
         send_servo_commands(pi_board, pwm_params, servo_params, controller.joint_angles)
-
+        msg = values.get()
+        controller.movement_reference.v_xy_ref = np.array([msg["x"], msg["y"]])
+        controller.movement_reference.wz_ref = msg["twist"]
         while now - last_loop < controller.gait_params.dt:
             now = time.time()
         # print("Time since last loop: ", now - last_loop)
