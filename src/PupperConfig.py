007ec867b0e92e99fc912cfa061999c6aca18d73
Nathan Kau <nathankau@gmail.com>
2019-10-27 19:23:55 -0700

    Updated calibration

--- src/PupperConfig.py
+++ src/PupperConfig.py
@@ -3,6 +3,32 @@ import numpy as np
 from scipy.linalg import solve
 
 
+class PWMParams:
+    def __init__(self):
+        self.pins = np.array([[2, 17, 21, 6], [3, 27, 16, 5], [4, 22, 20, 12]])
+        self.range = 4000
+        self.freq = 250
+
+
+class ServoParams:
+    def __init__(self):
+        self.neutral_position_pwm = 1500  # Middle position
+        self.micros_per_rad = 11.4 * 180.0 / np.pi  # Must be calibrated
+
+        # The neutral angle of the joint relative to the modeled zero-angle in degrees, for each joint
+        self.neutral_angle_degrees = np.array(
+            [[8, 3, 0, 0], [45, 48, 45, 45], [-50, -38, -45, -45]]
+        )
+
+        self.servo_multipliers = np.array(
+            [[1, 1, 1, 1], [-1, 1, 1, -1], [1, -1, 1, -1]]
+        )
+
+    @property
+    def neutral_angles(self):
+        return self.neutral_angle_degrees * np.pi / 180.0  # Convert to radians
+
+
 class MovementCommand:
     """Stores movement command 
     """
