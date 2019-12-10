a2f5a6b1e2d6f7a7920e75b69f5d2b978d4c05e1
Stuart Bowers <stuart.bowers@gmail.comq>
2019-12-10 03:16:00 +0000

    Add ability to change stance height

--- run_robot.py
+++ run_robot.py
@@ -42,6 +42,14 @@ def main():
     last_loop = time.time()
     now = last_loop
     start = time.time()
+
+    gait_mode = 0  # 0 for non-walking, 1 for walking
+    prev_gait_toggle = 0
+
+    non_walking_gait= np.array([[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]])
+    walking_gait = np.array([[1, 1, 1, 0], [1, 0, 1, 1], [1, 0, 1, 1], [1, 1, 1, 0]])
+ 
+
     for i in range(60000):
         last_loop = time.time()
         step_controller(controller)
@@ -49,20 +57,34 @@ def main():
 
         try:
             msg = values.get()
-            print(msg)
         except UDPComms.timeout:
-            print("timout")
-            msg = {"x": 0, "y": 0, "twist": 0, "pitch": 0}
+            print("timeout")
+            msg = {"x": 0, "y": 0, "twist": 0, "pitch": 0, "gait_toggle": 0, "stance_movement": 0}
         x_vel = msg["y"] / 7.0
         y_vel = -msg["x"] / 7.0
         yaw_rate = -msg["twist"] * 0.8
+        gait_toggle = msg["gait_toggle"]
+        stance_movement = msg["stance_movement"]
+
+        # Check for gait toggle
+        if prev_gait_toggle == 0 and gait_toggle == 1:
+            gait_mode = not gait_mode
+        prev_gait_toggle = gait_toggle
 
         pitch = msg["pitch"] * 30.0 * np.pi / 180.0
-        print(pitch)
 
         controller.movement_reference.v_xy_ref = np.array([x_vel, y_vel])
         controller.movement_reference.wz_ref = yaw_rate
         controller.movement_reference.pitch = pitch
+
+        if gait_mode == 0: 
+            controller.gait_params.contact_phases = non_walking_gait
+        else:  
+            controller.gait_params.contact_phases = walking_gait
+
+        # Note this is negative since it is the feet relative to the body
+        controller.movement_reference.z_ref -= 0.001 * stance_movement
+
         while now - last_loop < controller.gait_params.dt:
             now = time.time()
         # print("Time since last loop: ", now - last_loop)
